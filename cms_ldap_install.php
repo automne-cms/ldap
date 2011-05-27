c92296a03dff0a7a3ce6b7aa9d0d016a7d7b2041
sebastien-pauchet <seb@automne-cms.org>
2011-05-27 14:11:44 +0200

    Correct a bug in install process

--- cms_ldap_install.php
+++ cms_ldap_install.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__).'/../../cms_rc_admin.php');
 //check if module is already installed (if so, it is an update)
 $installed = false;
 $codenames = CMS_modulesCatalog::getAllCodenames(true);
-if (isset($modules['cms_ldap'])) {
+if (isset($codenames['cms_ldap'])) {
 	$installed = true;
 }
 if (!$installed) {
